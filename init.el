82a95d71a94ef7f6205874ec8ad1e9fe7a42f7b8
Abhik Khanra <abhik.rk@gmail.com>
2019-10-05 15:17:05 +0530

    Install magit

--- init.el
+++ init.el
@@ -29,5 +29,9 @@
 (setq tab-width 2)
 (defvaralias 'c-basic-offset 'tab-width)
 (defvaralias 'cperl-indent-level 'tab-width)
-    
+
+(use-package magit
+  :ensure t)
+
+(global-set-key (kbd "s-m") 'magit-status)
     
