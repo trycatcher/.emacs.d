2aaf37f155cf6d7fe1a6df2c091222eb6573a6d5
Abhik Khanra <abhik.rk@gmail.com>
2019-10-05 12:50:42 +0530

    Install use-package to bootstrap other packages

--- init.el
+++ init.el
@@ -11,3 +11,14 @@
 ;; Move custom configuration variables set by Emacs, to a seperate file
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'noerror)
+
+(package-initialize)
+
+;; Install use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-verbose t)
+;; The :ensure keyword causes the package(s) to be installed automatically if not already present on your system:
+(setq use-package-always-ensure t)
