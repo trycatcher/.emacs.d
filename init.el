d2f97bfe66b80d112331e368a2c2f4394c5cdfaa
Abhik Khanra <abhik.rk@gmail.com>
2019-10-05 10:18:00 +0530

    Add global defaults
    FIXME: Not sure about delete-selection-mode yet, hence comemnted

--- init.el
+++ init.el
@@ -1,2 +1,9 @@
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
+
+(global-auto-revert-mode 1)
+(global-hl-line-mode 1)
+(global-linum-mode 1)
+(global-visual-line-mode 1)
+(column-number-mode 1)
+; (delete-selection-mode 1)
